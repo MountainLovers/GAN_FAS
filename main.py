5a0b8981c68056426df3888f9669ae05e6413d7f
Zhang Zongjie <zzjzxh0870@gmail.com>
2022-12-28 06:00:24 +0000

    feature: 重新设计了log输出和tensorboard，能够查看train_loss和val_loss，test_loss

--- main.py
+++ main.py
@@ -56,13 +56,13 @@ if __name__ == '__main__':
     test_file_list = opt.test_file_list
     model = FaceModel(opt,isTrain = True,input_nc = 3)
     test_data_loader = DataLoader(AlignedDataset(test_file_list,isTrain = False), batch_size=test_batch_size,
-                                   shuffle=True, num_workers=1,drop_last=False)
+                                   shuffle=True, num_workers=1,drop_last=True)
     dev_data_loader = DataLoader(AlignedDataset(dev_file_list,isTrain = False), batch_size=test_batch_size,
-                                   shuffle=True, num_workers=1,drop_last=False)
+                                   shuffle=True, num_workers=1,drop_last=True)
 
     train_dataset = AlignedDataset(train_file_list) 
     train_data_loader = DataLoader(train_dataset, batch_size=train_batch_size,
-                                    shuffle = True,num_workers=1,drop_last=False)
+                                    shuffle = True,num_workers=1,drop_last=True)
 
     writer.iter = 0
     for e in range(opt.epoch):
@@ -86,13 +86,13 @@ if __name__ == '__main__':
             pad_meter_train.update(model.label.cpu().data.numpy(),
                              class_output.cpu().data.numpy())
 
-            writer.add_scalars('C', {'C': model.get_current_losses()['C']}, i+ len(train_data_loader) *e)
-            writer.add_scalars('G_GAN', {'G_GAN_loss': model.get_current_losses()['G_GAN']}, i+ len(train_data_loader) *e)
-            writer.add_scalars('G_NP', {'G_NP_loss': model.get_current_losses()['G_NP']}, i+ len(train_data_loader) *e)
-            writer.add_scalars('D_real', {'D_real_loss': model.get_current_losses()['D_real']}, i+ len(train_data_loader) *e)
-            writer.add_scalars('D_fake', {'D_fake_loss': model.get_current_losses()['D_fake']}, i+ len(train_data_loader) *e)
-            writer.add_scalars('D', {'D': model.get_current_losses()['D']}, i+ len(train_data_loader) *e)
-            writer.add_scalars('G', {'G': model.get_current_losses()['G']}, i+ len(train_data_loader) *e)
+            writer.add_scalars('train_loss/C', {'C': model.get_current_losses()['C']}, i+ len(train_data_loader) *e)
+            writer.add_scalars('train_loss/G', {'G_GAN_loss': model.get_current_losses()['G_GAN']}, i+ len(train_data_loader) *e)
+            writer.add_scalars('train_loss/G', {'G_NP_loss': model.get_current_losses()['G_NP']}, i+ len(train_data_loader) *e)
+            writer.add_scalars('train_loss/D', {'D_real_loss': model.get_current_losses()['D_real']}, i+ len(train_data_loader) *e)
+            writer.add_scalars('train_loss/D', {'D_fake_loss': model.get_current_losses()['D_fake']}, i+ len(train_data_loader) *e)
+            writer.add_scalars('train_loss/D', {'D': model.get_current_losses()['D']}, i+ len(train_data_loader) *e)
+            writer.add_scalars('train_loss/G', {'G': model.get_current_losses()['G']}, i+ len(train_data_loader) *e)
             if i %100 ==0:
                 pad_meter_train.get_eer_and_thr()
                 pad_meter_train.get_hter_apcer_etal_at_thr(pad_meter_train.threshold)
@@ -104,9 +104,9 @@ if __name__ == '__main__':
                 logging.info(model.get_current_losses())
                 logging.info('HTER {pad_meter.hter:.4f} EER {pad_meter.eer:.4f} ACC {pad_meter.accuracy:.4f}'.format(
                     pad_meter=pad_meter_train))
-                logger.info("batch {}: {}".format(i, model.get_current_losses()))
-                logger.info('batch {}: HTER {pad_meter.hter:.4f} EER {pad_meter.eer:.4f} ACC {pad_meter.accuracy:.4f}'.format(
-                    i, pad_meter=pad_meter_train))
+                logger.info("Epoch [{}/{}] iter {}: train_loss: {}".format(e, opt.epoch, i, model.get_current_losses()))
+                logger.info('Epoch [{}/{}] iter {}: train HTER {pad_meter.hter:.4f} EER {pad_meter.eer:.4f} ACC {pad_meter.accuracy:.4f}'.format(
+                    e, opt.epoch, i, pad_meter=pad_meter_train))
 
                 # save sigal figure
                 plt.figure(dpi=300)
@@ -124,7 +124,7 @@ if __name__ == '__main__':
                 plt.savefig("%s/epoch_%d.png" % (img_save_dir, e))
 
                 plt.close()
-                logger.info("epoch {} batch {}: save figs ok".format(e, i))
+                logger.trace("Epoch [{}] iter {}: save figs ok".format(e, i))
 
                 # vutils.save_image(ret['fake_B'], "%s/epoch_%d_fake.png" % (img_save_dir, e), normalize=True)
                 # vutils.save_image(ret['real_B'], "%s/epoch_%d_real.png" % (img_save_dir, e), normalize=True)
@@ -133,7 +133,24 @@ if __name__ == '__main__':
         if e%1==0:
             model.eval()
             pad_dev_mater = eval_model(dev_data_loader,model)
+            logger.info("Epoch [{}/{}] val_loss: {}".format(e, opt.epoch, model.get_current_losses()))
+            writer.add_scalars('val_loss/C', {'C': model.get_current_losses()['C']}, e)
+            writer.add_scalars('val_loss/G', {'G_GAN_loss': model.get_current_losses()['G_GAN']}, e)
+            writer.add_scalars('val_loss/G', {'G_NP_loss': model.get_current_losses()['G_NP']}, e)
+            writer.add_scalars('val_loss/D', {'D_real_loss': model.get_current_losses()['D_real']}, e)
+            writer.add_scalars('val_loss/D', {'D_fake_loss': model.get_current_losses()['D_fake']}, e)
+            writer.add_scalars('val_loss/D', {'D': model.get_current_losses()['D']}, e)
+            writer.add_scalars('val_loss/G', {'G': model.get_current_losses()['G']}, e)
+
             pad_meter = eval_model(test_data_loader,model)
+            logger.info("Epoch [{}/{}] test_loss: {}".format(e, opt.epoch, model.get_current_losses()))
+            writer.add_scalars('test_loss/C', {'C': model.get_current_losses()['C']}, e)
+            writer.add_scalars('test_loss/G', {'G_GAN_loss': model.get_current_losses()['G_GAN']}, e)
+            writer.add_scalars('test_loss/G', {'G_NP_loss': model.get_current_losses()['G_NP']}, e)
+            writer.add_scalars('test_loss/D', {'D_real_loss': model.get_current_losses()['D_real']}, e)
+            writer.add_scalars('test_loss/D', {'D_fake_loss': model.get_current_losses()['D_fake']}, e)
+            writer.add_scalars('test_loss/D', {'D': model.get_current_losses()['D']}, e)
+            writer.add_scalars('test_loss/G', {'G': model.get_current_losses()['G']}, e)
 
             pad_meter.get_eer_and_thr()
             pad_dev_mater.get_eer_and_thr()
@@ -143,9 +160,8 @@ if __name__ == '__main__':
             logging.info("epoch %d test"%e)
             logging.info('HTER {pad_meter.hter:.4f} EER {pad_meter.eer:.4f} ACC {pad_meter.accuracy:.4f}'.format(
                 pad_meter=pad_meter))
-            logger.info("epoch %d test"%e)
-            logger.info('HTER {pad_meter.hter:.4f} EER {pad_meter.eer:.4f} ACC {pad_meter.accuracy:.4f}'.format(
-                pad_meter=pad_meter))
+            logger.info('Epoch [{}/{}]: test HTER {pad_meter.hter:.4f} EER {pad_meter.eer:.4f} ACC {pad_meter.accuracy:.4f}'.format(
+                e, opt.epoch, pad_meter=pad_meter))
             is_best = pad_meter.hter <= best_res
             best_res = min(pad_meter.hter, best_res)
             if is_best:
