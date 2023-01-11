1aa2a804b3ddbb050a753c03c6a1ed8b9a67c83b
Zhang Zongjie <zongjie.z@stu.pku.edu.cn>
2023-01-11 13:40:34 +0000

    feature: 在wgan上增加了CBAM模块

--- networks.py
+++ networks.py
@@ -152,11 +152,21 @@ class Encoder(nn.Module):
         self.down2 = DOWN(128, 128)
         self.down3 = DOWN(128, 128)
 
+        self.ca1 = ChannelAttention(in_planes=64)
+        self.sa1 = SpatialAttention()
+
+        self.ca3 = ChannelAttention(in_planes=128)
+        self.sa3 = SpatialAttention()
 
     def forward(self, x):
         dx1 = self.inc(x)
+        dx1 = self.ca1(dx1) * dx1
+        dx1 = self.sa1(dx1) * dx1
+
         dx2 = self.down1(dx1)
         dx3 = self.down2(dx2)
+        dx3 = self.ca3(dx3) * dx3
+        dx3 = self.sa3(dx3) * dx3
         dx4 = self.down3(dx3)
 
         re_dx2 = F.adaptive_avg_pool2d(dx2, 32)
@@ -167,6 +177,36 @@ class Encoder(nn.Module):
         return catfeat, dx4
 
 
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+           
+        self.mlp = nn.Sequential(nn.Conv2d(in_planes, in_planes // 16, 1, bias=True),
+                               nn.ReLU(),
+                               nn.Conv2d(in_planes // 16, in_planes, 1, bias=True))
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.mlp(self.avg_pool(x))
+        max_out = self.mlp(self.max_pool(x))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
 
 class FeatEmbedder(nn.Module):
     def __init__(self, in_channels=128):
